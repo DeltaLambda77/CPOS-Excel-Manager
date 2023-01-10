3da3b8a6f2bd4bb973abb3aa19e9e0ac9a853bea
danlozano77 <danlozano77.gmail.com>
2023-01-09 20:39:18 -0500

    first commit

---
+++ CPOS Excel Manager/Commands/UpdateViewCommand.cs
@@ -0,0 +1,45 @@
+﻿using CPOS_Excel_Manager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CPOS_Excel_Manager.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Trace.WriteLine(parameter.ToString());
+
+            if (parameter.ToString() == "Home")
+            {
+                viewModel.SelectedViewModel = new HomeViewModel();
+            }
+            else if (parameter.ToString() == "Account")
+            {
+                viewModel.SelectedViewModel = new AccountViewModel();
+
+            }
+
+        }
+    }
+}
---
+++ CPOS Excel Manager/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CPOS_Excel_Manager.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CPOS_Excel_Manager.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel;
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set 
+            { 
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
